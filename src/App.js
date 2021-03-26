bf98f79f1e09589cece334c4a9a665595b667a38
Ahmad Bappy <athb.001@gmail.com>
2021-03-26 09:11:23 +0600

    Basic (props)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'photoshop', price: '$99.99' },
+    { name: 'illustrator', price: '$89.99' },
+    { name: 'premire pro', price: '$199.99' },
+    { name: 'lighthouse', price: '$59.99' },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Product name={products[0].name} price={products[0].price}></Product> */}
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+        <Product product={products[3]}></Product>
       </header>
     </div>
   );
+}
+
+function Product(props) {
+  const productStyle = {
+    margin: '5px',
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '225px',
+    width: '225px',
+    float: 'left',
+    color: 'black',
+    display: 'block'
+  }
+  const { name, price } = props.product;
+  // console.log(name, price);
+  return (
+    <div style={productStyle}>
+      <h5>{name}</h5>
+      <h3>{price}</h3>
+      <button>Buy now</button>
+    </div>
+  )
 }
 
 export default App;
