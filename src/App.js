5ec123e6e0fda01fdb59961202b4d15256861375
Ahmad Bappy <athb.001@gmail.com>
2021-03-26 14:56:10 +0600

    useState() & useEffect()

--- src/App.js
+++ src/App.js
@@ -1,56 +1,127 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 
 // render function
 function App() {
-  const girls = [
-    { name: 'Shochtika', age: 29, status: 'Marraied' },
-    { name: 'Poroma', age: 35, status: 'Widowed' },
-    { name: 'Oishorjo', age: 21, status: 'single' },
-    { name: 'Dipa', age: 24, status: 'Open-minded' },
-    { name: 'Nipa', age: 42, status: 'Aunte' },
-    { name: 'Dhvani', age: 22, status: 'Crush' },
-  ];
   return (
     <div className="App">
       <header className="App-header">
-        {
-          girls.map(option => <DateGirls girl={option}></DateGirls>)
-        }
+
+        <Counter></Counter>
+        <Users></Users>
+
       </header>
     </div>
   )
 }
 
-// component
-function DateGirls(props) {
-  const girlsProfile = {
-    margin: '5px',
-    border: '1px solid orange',
-    borderRadius: '5px',
-    backgroundColor: 'pink',
-    height: '300px',
-    width: '250px',
-    float: 'left',
-    color: 'black',
-    display: 'block'
-  }
-  const buttonDesign = {
-    borderRadius: '20px',
-    backgroundColor: 'lemon'
-  }
-  const { name, age, status } = props.girl;
+function Users() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
   return (
-    <div style={girlsProfile}>
-      <h2>{name}</h2>
-      <h5>{age}</h5>
-      <p>{status}</p>
-      <button style={buttonDesign}>Ask for a date</button>
+    <div>
+      <h3>- Dynamic Users -</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  // Basic way ...
+  // const handleIncrease = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // }
+  // const handleDecrease = () => {
+  //   const newCount = count - 1;
+  //   setCount(newCount);
+  // }
+
+  // Intermediate way ...
+  // const handleIncrease = () => setCount(count + 1)
+  // const handleDecrease = () => setCount(count - 1)
+
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button> {/*advanced way*/}
+      <button onMouseMove={() => setCount(count - 1)}>Decrease</button> {/*advanced way*/}
     </div>
   )
 }
 
 export default App;
+
+
+
+
+// ------------------------------***********------------------------------------
+//  render function ....
+// function App() {
+//   const girls = [
+//     { name: 'Shochtika', age: 29, status: 'Marraied' },
+//     { name: 'Poroma', age: 35, status: 'Widowed' },
+//     { name: 'Oishorjo', age: 21, status: 'single' },
+//     { name: 'Dipa', age: 24, status: 'Open-minded' },
+//     { name: 'Nipa', age: 42, status: 'Aunte' },
+//     { name: 'Dhvani', age: 22, status: 'Crush' },
+//   ];
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+
+//         {/* {
+//           girls.map(option => <DateGirls girl={option}></DateGirls>)
+//         } */}
+// {/* 
+// //         <Counter></Counter>
+// //       </header>
+
+// //     </div> */}
+// //   )
+// // }
+
+// {/* // component ....
+// // function DateGirls(props) { */}
+//   const girlsProfile = {
+//     margin: '5px',
+//     border: '1px solid orange',
+//     borderRadius: '5px',
+//     backgroundColor: 'pink',
+//     height: '300px',
+//     width: '250px',
+//     float: 'left',
+//     color: 'black',
+//     display: 'block'
+//   }
+//   const buttonDesign = {
+//     borderRadius: '20px',
+//     backgroundColor: 'lemon'
+//   }
+//   const { name, age, status } = props.girl;
+//   return (
+//     <div style={girlsProfile}>
+//       <h2>{name}</h2>
+//       <h5>{age}</h5>
+//       <p>{status}</p>
+//       <button style={buttonDesign}>Ask for a date</button>
+//     </div>
+//   )
+// }
+// ----------------------------------------******-------------------------------
+
 
 
 
